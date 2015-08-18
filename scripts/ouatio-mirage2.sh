af75e78a9c4f03f5857f8b952fedddfdae125fc3
Logan Cox <lmc@ou.edu>
2015-08-17 21:30:51 -0500

    started on mirage2 config

---
+++ scripts/ouatio-mirage2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+
+curl https://raw.githubusercontent.com/creationix/nvm/v0.5.1/install.sh | sh
+source ~/.profile
+
+# Install node bower and grunt
+nvm install 0.10.31
+nvm alias default 0.10.31
+npm install -g bower
+npm install -g grunt && npm install -g grunt-cli
+
+#OK. fine. ruby.
+gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable --ruby
+source ~/.profile
+gem install sass -v 3.3.14
+gem install compass -v 1.0.1

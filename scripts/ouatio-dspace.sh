78b6ec5d0abd6c1af308fd0392b795b1c27b248b
Logan Cox <lmc@ou.edu>
2015-08-17 20:08:09 -0500

    updating the dspace install

--- scripts/ouatio-dspace.sh
+++ scripts/ouatio-dspace.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-
-#make the dspace web app location 
+# make the dspace web app location 
 mkdir -p  /srv/ouatio/
 
 # point tomcat at the dspace web app location
 cp -r /vagrant/etc/Catalina/localhost/*.xml /usr/share/tomcat/conf/Catalina/localhost
 
+# build the dspace install as the vagrant user
+cd /home/vagrant/ouatio_dspace 
+sudo -u vagrant  mvn -Dmaven.repo.local=/vagrant/m2 package
 
+# install the files as root
+cd /home/vagrant/ouatio_dspace/dspace/target/dspace-installer
+ant fresh_install
 
-# cd ./ouatio_dspace
-# mvn -Dmaven.repo.local=/vagrant/m2  package
+#tomcat needs to own folders it will be writing to
+sudo chown -R tomcat:tomcat assetstore/ solr/
 
-# cd ./dspace/target/dspace-installer
-# ant fresh_install
-
-
-
-
-
-# systemctl restart tomcat.service
+# restart tomcat
+systemctl restart tomcat
 
 
 

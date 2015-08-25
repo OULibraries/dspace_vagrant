c671cb92eb53464435093f46c342cd1b2ded6403
Logan Cox <lmc@ou.edu>
2015-08-24 19:11:32 -0500

    amazon RDS needs PostgreSQL 9.4

--- scripts/ouatio-db-amazon.sh
+++ scripts/ouatio-db-amazon.sh
@@ -7,7 +7,12 @@ DB_ADMIN=
 DB_NAME=
 
 # DSpace Database Installation and Setup
-yum install -y postgresql postgresql-jdbc
+# yum install -y postgresql postgresql-jdbc
+
+yum install http://yum.postgresql.org/9.4/redhat/rhel-6-x86_64/pgdg-redhat94-9.4-1.noarch.rpm
+yum install -y postgresql94 postgresql94-jdbc
+
+
 
 # Create the dspace db user and database
 # requires interactive run to specify passwords

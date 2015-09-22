ed0f662523d4746950547654b3aa27da6456be2d
Logan Cox <lmc@ou.edu>
2015-09-22 18:27:27 -0500

    need to update discovery index, too

--- scripts/shareok-db.sh
+++ scripts/shareok-db.sh
@@ -2,8 +2,15 @@
 . /vagrant/etc/conf.sh
 
 
+
+SHAREOK_EXPORT=/vagrant/downloads/shareokprod-20150922-dump.sql
+
+
+
+# Remove existing DB and install fresh export
 $DSPACE_RUN/bin/dspace database clean
+psql -U $DB_NAME -h $DB_HOST $DB_NAME < $SHAREOK_EXPORT
 
-psql -U $DB_NAME -h $DB_HOST $DB_NAME < /vagrant/downloads/shareokprod-20150922-dump.sql
-
+# upgrade shareok db and rebuild indexes
 $DSPACE_RUN/bin/dspace database migrate
+$DSPACE_RUN/bin/dspace  index-discovery -b

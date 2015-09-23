431520f1ea7f1bc7ed4a114d404c724d85a601ea
vagrant <vagrant@dspace.vm.test>
2015-09-22 22:38:24 -0500

    more upgrade steps.

--- scripts/shareok-db.sh
+++ scripts/shareok-db.sh
@@ -1,16 +1,24 @@
-
+#
+# Load a copy of the SHAREOK db and then upgrade it 
+#
 . /vagrant/etc/conf.sh
-
-
-
 SHAREOK_EXPORT=/vagrant/downloads/shareokprod-20150922-dump.sql
-
-
 
 # Remove existing DB and install fresh export
 $DSPACE_RUN/bin/dspace database clean
 psql -U $DB_NAME -h $DB_HOST $DB_NAME < $SHAREOK_EXPORT
 
-# upgrade shareok db and rebuild indexes
+# Upgrade Database
 $DSPACE_RUN/bin/dspace database migrate
+
+# Remove deprecated Database Browse Tables
+$DSPACE_RUN/bin/dspace index-db-browse -f -d
+
+# Delete contents of legacy browse table
+cat <<EOF | psql -U $DB_NAME -h $DB_HOST $DBNAME
+DELETE FROM COMMUNITIES2ITEM
+EOF
+
+
+# update the discovery index with the new data
 $DSPACE_RUN/bin/dspace  index-discovery -b

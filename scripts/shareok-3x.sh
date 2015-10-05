5babc6c4a765d12c1aae04100a2cf37b393b00a8
Logan Cox <lmc@ou.edu>
2015-10-05 15:36:08 -0500

    updated scp comment to show more things being copied

--- scripts/shareok-3x.sh
+++ scripts/shareok-3x.sh
@@ -8,7 +8,7 @@ mkdir -p /vagrant/downloads
 
 ## maven artifacts and git credentials required to checkout and build
 ## dspace 3.x stored at https://atmire.com/git/clients/oklahoma
-# scp -F /vagrant/etc/ssh_config shareok-prod:/srv/migrate/dotstuff.tar /vagrant/downloads
+# scp -F shareok-prod:/srv/migrate/* /vagrant/downloads
 
 sudo -u vagrant tar -xovf /vagrant/downloads/dotstuff.tar  -C /home/vagrant
 mv /home/vagrant/.m2 /vagrant/m2

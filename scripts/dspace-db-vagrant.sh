3003a1f2bf17193e84179e96306659aec427f126
Logan Cox <lmc@ou.edu>
2015-10-07 15:54:41 -0500

    organizing database setup

--- scripts/dspace-db-vagrant.sh
+++ scripts/dspace-db-vagrant.sh
@@ -3,9 +3,8 @@
 # DSpace Server Installation and Setup
 . /vagrant/etc/shareok.conf.sh
 
+
 # create admin user, and dspace db
 cat <<EOF | su - postgres -c psql
-CREATE USER $DB_ADMIN WITH PASSWORD $DB_ADMIN_PASS SUPERUSER;
-CREATE USER $DB_NAME WITH PASSWORD '$DB_PASS';
-CREATE DATABASE $DB_NAME WITH OWNER=$DB_NAME ENCODING='UNICODE'
+CREATE USER $DB_ADMIN WITH PASSWORD '$DB_ADMIN_PASS' SUPERUSER;
 EOF

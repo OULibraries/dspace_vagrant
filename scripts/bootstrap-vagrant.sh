53ff8c853a706d786f8ccc4ab74f99a7a4fb0903
Jason Sherman <jsn.sherman@gmail.com>
2015-10-07 12:59:30 -0500

    Gave provisioning more Ansible!

--- scripts/bootstrap-vagrant.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-# Install an ntpd that works well with vms 
-yum install -y chrony
-
--- scripts/shareok-3x.sh
+++ scripts/shareok-3x.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-#
 # Load a copy of the SHAREOK db and then upgrade it 
-#
 . /vagrant/etc/conf.sh
 
-mkdir -p /vagrant/downloads
-
-
 ## maven artifacts and git credentials required to checkout and build
-## dspace 3.x stored at https://atmire.com/git/clients/oklahoma
-# scp -F shareok-prod:/srv/migrate/* /vagrant/downloads
 
 sudo -u vagrant tar -xovf /vagrant/downloads/dotstuff.tar  -C /home/vagrant
 rsync -rv  /home/vagrant/.m2/  /vagrant/m2
-
-
-
-

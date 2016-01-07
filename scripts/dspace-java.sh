86f969db1496e7e5946e0dbf5cce19bb26f4dd3f
Logan Cox <lmc@ou.edu>
2016-01-07 11:56:38 -0600

    need to configure tomcat AFTER installing it

--- scripts/dspace-java.sh
+++ scripts/dspace-java.sh
@@ -4,10 +4,12 @@
 
 # Java, Tomcat and Related Tools
 
-cp /vagrant/etc/tomcat.conf /etc/tomcat/tomcat.conf
+
 
 yum install -y java-1.8.0-openjdk maven ant
 yum install -y tomcat tomcat-webapps tomcat-admin-webapps 
+
+cp /vagrant/etc/tomcat.conf /etc/tomcat/tomcat.conf
 
 systemctl enable tomcat.service
 systemctl start tomcat

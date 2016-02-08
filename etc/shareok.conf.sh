8e5a57e85feee681937252f467b6043aaa9151aa
Logan Cox <lmc@ou.edu>
2016-02-08 14:04:14 -0600

    Moved code for reading config of db to export.
    This needs to live in the export script because other scripts need to
    be able to run when the target config doesn't exist yet.

--- etc/shareok.conf.sh
+++ etc/shareok.conf.sh
@@ -23,11 +23,3 @@ DB_PASS=dspace
 
 DB_EXPORT="/vagrant/downloads/shareok-${MAVEN_PROFILE}-dump.sql"
 
-# Read database config from dspace config
-DS_CFG="${DSPACE_RUN}/config/dspace.cfg"
-DS_DB_URL=`cat $DS_CFG | grep ^db.url | cut -d "=" -f 2 | xargs`
-DS_DB_HOST=`echo $DS_DB_URL | cut -d "/" -f 3 | cut -d ":" -f 1`
-DS_DB_PORT=`echo $DS_DB_URL | cut -d "/" -f 3 | cut -d ":" -f 2`
-DS_DB=`echo $DS_DB_URL | cut -d "/" -f 4 | xargs`
-DS_DB_USER=`cat $DS_CFG | grep ^db.username | cut -d "=" -f 2 | xargs`
-DS_DB_PASS=`cat $DS_CFG | grep ^db.password | cut -d "=" -f 2 | xargs`
